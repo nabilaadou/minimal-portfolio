fb6dc6db4afd714d7bb64c3ac313699a9079702e
nabilaadou <nabilaadou@gmail.com>
2025-09-28 14:02:14 +0100

    adding gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "./src/public"
+  publicDir: "./src/public",
+  base: '/minimal-portfolio/'
 })
