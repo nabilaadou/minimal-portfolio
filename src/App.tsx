b8c93e4333d2d6c51f5361c113ce95fd0874bd4e
nabilaadou <nabilaadou@gmail.com>
2025-09-28 15:03:53 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { Project2 } from './pages/Project2'
 
 export default function App() {
 	return (
-	  <BrowserRouter basename="/minimal-portfolio">
-	  	<Routes>
-	      	<Route path='/' element={<Landingpage />} />
-	      	<Route path='/projects/ft_transcendance' element={<Project3 />} />
-	      	<Route path='/projects/codeVisualizer' element={<Project2 />} />
-	    	</Routes>
-	  </BrowserRouter>
+		<div>
+			<p>test</p>
+			<BrowserRouter basename="/minimal-portfolio">
+				<Routes>
+					<Route path='/' element={<Landingpage />}/>
+					<Route path='/projects/ft_transcendance' element={<Project3 />}/>
+					<Route path='/projects/codeVisualizer' element={<Project2 />}/>
+				</Routes>
+			</BrowserRouter>
+		</div>
 	)
 }
