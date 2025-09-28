72c79f35e6a22868b7f4835c6d08e2f1c8831651
nabilaadou <nabilaadou@gmail.com>
2025-09-28 15:06:01 +0100

    updated dist

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import { Project2 } from './pages/Project2'
 export default function App() {
 	return (
 		<div>
-			<p>test</p>
-			<BrowserRouter basename="/minimal-portfolio">
+			<Landingpage />
+			{/* <BrowserRouter basename="/minimal-portfolio">
 				<Routes>
 					<Route path='/' element={<Landingpage />}/>
 					<Route path='/projects/ft_transcendance' element={<Project3 />}/>
 					<Route path='/projects/codeVisualizer' element={<Project2 />}/>
 				</Routes>
-			</BrowserRouter>
+			</BrowserRouter> */}
 		</div>
 	)
 }
